09ec25ca773939e547cbdc6f53d1413a37e99a59
Syed Affan Hameed <syedaffanhameed@gmail.com>
2024-12-25 15:38:45 +0530

    learning about containers

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,25 @@ function App() {
 
   return (
     <>
-      <div className="bg-green-400  container mx-auto">
-        <h1> This is SetCount </h1>
-      </div>
-      <div className="bg-blue-300 size-96 relative">
-        <div className="bg-blue-600 size-16 absolute right-0"></div>
-        <div className="bg-red-600 size-16 absolute left-0 top-0"></div>
-        <div className="bg-yellow-600 size-16 absolute left-0 bottom-0"></div>
-        <div className="bg-violet-600 size-16 absolute right-0 bottom-0"></div>
-      </div>
+     <div className="bg-black ">
+    <ul className="flex ">
+      <li><a href="#" className="text-white p-3 flex hover:bg-slate-600">Home</a></li>
+      <li><a href="#" className="text-white p-3 flex hover:bg-slate-600">News</a></li>
+      <li><a href="#" className="text-white p-3 flex hover:bg-slate-600">Contact</a></li>
+      <li><a href="#" className="text-white p-3 flex hover:bg-slate-600">About</a></li>
+    </ul>
+     </div>
+     <div className="container mx-auto">
+     <div className="bg-blue-500 mx-auto h-20  m-2">
+      <h1>this is the first container div</h1>
+     </div>
+     <div className="bg-blue-800 mx-auto h-20 m-2">
+      <h1>this is the first container div</h1>
+     </div>
+     <div className="bg-violet-800 mx-auto h-20 m-2">
+      <h1>this is the first container div</h1>
+     </div>
+     </div>
     </>
   );
 }
