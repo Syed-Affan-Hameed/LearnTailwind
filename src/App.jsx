e019156dd3b521bca6d4f98e6b1275dd5109add4
Syed Affan Hameed <syedaffanhameed@gmail.com>
2024-12-25 13:35:21 +0530

    learning hieght and width

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
 
   return (
     <>
+    <div className="h-screen">
+
     <div className="bg-black flex">
       <ul className="flex text-white [&>*]:flex">
         <li><a className="p-3 hover:bg-zinc-400" href="">Home</a></li>
@@ -15,6 +17,20 @@ function App() {
         <li><a className="p-3 hover:bg-zinc-400" href="">Contact</a></li>
         <li><a className="p-3 hover:bg-zinc-400" href="">About us</a></li>
       </ul>
+    </div>
+   <div className="bg-blue-400 w-auto">
+    <h1>Learn Tailwind width</h1>
+   </div>
+   <div className="bg-blue-400 w-fit hover:w-1/2">
+    <h1>Learn Tailwind better width</h1>
+   </div>
+   <div className="bg-blue-400 h-10 min-h-7  hover:h-1/4">
+    <h1>Learn Tailwind better hieght</h1>
+   </div>
+
+   <div className="bg-blue-400 size-96 ">
+    <h1> Learn Tailwind size </h1>
+   </div>
     </div>
     </>
   );
